71c2a38003968b59be5e90d6cd9619c374b6f042
kero <kerolosehabgaber@gmail.com>
2026-04-15 23:28:32 +0200

    start  Categories CRUD Operations

--- models/categoryModel.js
+++ models/categoryModel.js
@@ -3,9 +3,20 @@ const mongoose = require('mongoose')
 
 //1-creaate schema
 const categorySchema = new mongoose.Schema({
-    name: String,
-    
-    
-})
+    name: {
+        type: String,
+        required: [true, 'Category is required'],
+        unique: [true, 'Category name must be unique'],
+        minlength: [3, 'Category name must be at least 3 characters'],
+        maxlength: [30, 'Category name must be at most 30 characters']
+    },
+    slug:{
+        type: String,
+        lowercase: true,
+    },    
+},
+{timestamps: true}
+
+)
 // 2- create model 
 const CategoryModel = mongoose.model('Category', categorySchema)
